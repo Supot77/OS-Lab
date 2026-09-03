a906b528adcf49fc0b8839667efe0c2a135cdcb1
Supot <supot5888@gmail.com>
2026-09-03 16:24:48 +0000

    Remove Lab 1 files from repository root

--- profiler.py
+++
@@ -1,7 +0,0 @@
-import os
-import psutil
-import platform
-
-print(f"OS Name: {platform.system()} {platform.release()}")
-print(f"Number of CPU Cores: {psutil.cpu_count(logical=True)}")
-print(f"Total RAM: {round(psutil.virtual_memory().total / (1024**3), 2)} GB")
--- stress_test.py
+++
@@ -1,5 +0,0 @@
-import math
-
-print("Starting CPU Stress Test... Press Ctrl+C to stop.")
-while True:
-    math.factorial(50000) # Intensive calculation
